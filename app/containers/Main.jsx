0467b97ca73010503dc5b9c44941119854e4a3ff
Jeonghyeon Kim <darkavengerk@gmail.com>
2019-11-07 06:36:06 +0900

    first page route

---
+++ app/containers/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/*
+ * Note: This is kept as a container-level component,
+ *  i.e. We should keep this as the container that does the data-fetching
+ *  and dispatching of actions if you decide to have any sub-components.
+ */
+const Main = () => <div>Welcome to the James An. Stay tuned...</div>;
+
+export default Main;
