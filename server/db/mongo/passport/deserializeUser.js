ef15cb634dcd67c48eaa116363bf505832ac75d9
jhkim <darkavengerk@gmail.com>
2019-12-31 11:05:47 +0900

    api fix

--- server/db/mongo/passport/deserializeUser.js
+++ server/db/mongo/passport/deserializeUser.js
@@ -3,8 +3,7 @@ import User from '../models/user';
 import { loginServer } from '../../../../config/secrets'
 
 export default (id, done) => {
-  console.log('fix the code below');
-  request(loginServer + `/external-login/${id}/takedown`, function (err, response, body) {
+  request(loginServer + `/external-user/${id}`, function (err, response, body) {
     done(err, User.wash(body));
   });
 };
