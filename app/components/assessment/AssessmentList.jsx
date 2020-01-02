b0b9662cb8316f9ff20e7ecaeafbb6422c1f77f6
jhkim <darkavengerk@gmail.com>
2020-01-02 14:32:55 +0900

    high-lighting

--- app/components/assessment/AssessmentList.jsx
+++ app/components/assessment/AssessmentList.jsx
@@ -25,6 +25,10 @@ class AssessmentComponent extends Component {
     return event => this.setState({mouseIndex: index});
   }
 
+  mouseOut(index) {
+    return event => this.setState({mouseIndex: -1});
+  }
+
   render() {
     const { exams, handleClick } = this.props;
     return (
@@ -32,19 +36,48 @@ class AssessmentComponent extends Component {
         <div className={cx('list-container')}>
           <div>
             <div className={cx('list-header', 'padding-left')} style={{width: '350px'}}>Name</div>
-          { exams && exams.map((exam, i) => <div className={cx('list-item', 'padding-left', {hover: i === this.state.mouseIndex})} key={exam._id} onClick={handleClick(exam)} onMouseOver={this.mouseOver(i)}>
-            {exam.student.name}</div>)
-          }
+            { exams && exams.map((exam, i) => (
+                <div
+                  className={cx('list-item', 'padding-left', {hover: i === this.state.mouseIndex})}
+                  key={exam._id} 
+                  onClick={handleClick(exam)} 
+                  onMouseOver={this.mouseOver(i)}
+                  onMouseOut={this.mouseOut(i)}
+                >
+                  {exam.student.name}
+                </div>
+              ))
+            }
           </div>
           <div>
             <div className={cx('list-header')} style={{width: '130px'}}>Grade</div>
-          { exams && exams.map((exam, i) => <div className={cx('list-item', {hover: i === this.state.mouseIndex})} key={exam._id} onClick={handleClick(exam)} onMouseOver={this.mouseOver(i)}>
-            {exam.student.grade}</div>)}
+            { exams && exams.map((exam, i) => (
+                <div
+                  className={cx('list-item', {hover: i === this.state.mouseIndex})}
+                  key={exam._id} 
+                  onClick={handleClick(exam)} 
+                  onMouseOver={this.mouseOver(i)}
+                  onMouseOut={this.mouseOut(i)}
+                >
+                  {exam.student.grade}
+                </div>
+              ))
+            }
           </div>
           <div>
             <div className={cx('list-header')} style={{width: '230px'}}>Registration Date</div>
-          { exams && exams.map((exam, i) => <div className={cx('list-item', {hover: i === this.state.mouseIndex})} key={exam._id} onClick={handleClick(exam)} onMouseOver={this.mouseOver(i)}>
-            {exam.created && String(exam.created).split('T')[0]}</div>)}
+            { exams && exams.map((exam, i) => (
+                <div
+                  className={cx('list-item', {hover: i === this.state.mouseIndex})}
+                  key={exam._id} 
+                  onClick={handleClick(exam)} 
+                  onMouseOver={this.mouseOver(i)}
+                  onMouseOut={this.mouseOut(i)}
+                >
+                  {exam.created && String(exam.created).split('T')[0]}
+                </div>
+              ))
+            }
           </div>
         </div>
       </div>
